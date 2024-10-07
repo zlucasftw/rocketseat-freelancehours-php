1a3b50feab8d9ec7018b1ba11ae80b005dab99aa
zlucasftw <ilucasmcpvp@gmail.com>
2024-10-07 13:25:46 -0300

    update: Aula 01 (wip)

---
+++ resources/views/livewire/teste.blade.php
@@ -0,0 +1,20 @@
+<div>
+    {{-- Success is as dangerous as failure. --}}
+
+    <h1>Meu componente livewire</h1>
+
+    <input wire:model.live="search" />
+
+    <br>
+
+    <ul>
+
+        @foreach($users as $user)
+
+        <li>{{ $user->name }}</li>
+
+        @endforeach
+
+    </ul>
+
+</div>
