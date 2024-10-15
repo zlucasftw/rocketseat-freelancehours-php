3a03e4f23c6b201cebb765a783317e9dba6a17d1
zlucasftw <ilucasmcpvp@gmail.com>
2024-10-15 02:42:42 -0300

    wip: Notificacoes

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -5,6 +5,8 @@ namespace App\Livewire\Proposals;
 use App\Actions\ArrangePositions;
 use App\Models\Project;
 use App\Models\Proposal;
+use App\Notifications\NewProposal;
+use App\Notifications\PerdeuMane;
 use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Rule;
 use Livewire\Component;
@@ -46,6 +48,8 @@ class Create extends Component
             $this->arrangePosition($proposal);
         });
 
+        $this->project->author->notify(new NewProposal($this->project));
+
         $this->dispatch('proposal::created');
 
         $this->modal = false;
@@ -66,7 +70,9 @@ class Create extends Component
 
         if ($otherProposal) {
             $proposal->update(['position_status' => 'up']);
-            Proposal::query()->where('id', '=', $otherProposal->id)->update(['position_status' => 'down']);
+            $oProposal = Proposal::find($otherProposal->id);
+            $oProposal->update(['position_status' => 'down']);
+            $oProposal->notify(new PerdeuMane($this->project));
         }
 
         ArrangePositions::run($proposal->id);
